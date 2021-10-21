3e01133866fc95e4ba8e5c858dc49ab3577f3776
Pedro <pmapm@ua.pt>
2021-10-21 17:10:15 +0100

    finished dockerfile getting started

---
+++ lab1/lab1_4/2-createtable.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS testtable;
+
+CREATE TABLE "testtable" (
+    id serial PRIMARY KEY NOT NULL;
+    name CHARACTER varying(30) NOT NULL;
+);
---
+++ lab1/lab1_4/3-insertdata.sql
@@ -0,0 +1,5 @@
+INSERT INTO testtable (name) 
+values
+("Jon Doe")
+("Jane Doe")
+("Waltr White")
