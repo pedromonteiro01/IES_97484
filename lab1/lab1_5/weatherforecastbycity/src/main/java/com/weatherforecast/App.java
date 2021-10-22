3c4e11cb46eff79cf58ac25da7c0cad74d2e0a95
Pedro <pmapm@ua.pt>
2021-10-22 18:43:41 +0100

    now using logger instead of print

--- lab1/lab1_5/weatherforecastbycity/src/main/java/com/weatherforecast/App.java
+++ lab1/lab1_5/weatherforecastbycity/src/main/java/com/weatherforecast/App.java
@@ -3,16 +3,23 @@ package com.weatherforecast;
 import com.ipmaapiclient.Main;
 import com.ipmaapiclient.CityForecast;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 public class App {
     public static void main(String[] args) {
+
+        final Logger logger = LogManager.getLogger(Main.class.getName());
+
         try {
             CityForecast api = Main.getAllData(args[0]);
-            System.out.println("Max temp for today:" + api.getTMax()+"ºC");
+            logger.info("Max temp for today:" + api.getTMax()+"ºC");
         } 
         
         catch (NullPointerException e) {
             // handle exception when api not working (or network is off)
-            System.out.println("API not working!");
+            logger.info("API not working!");
         }
     }
 }
