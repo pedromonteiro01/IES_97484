5da07fe323810ffac12a710ca5753d4c74494f2e
pedromonteiro01 <pmapm@ua.pt>
2021-11-11 10:56:49 +0000

    added project for lab3_2

---
+++ lab3/lab3_2/springboot_mysql/src/main/java/com/ies/ex2/SpringbootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.ies.ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlApplication.class, args);
+	}
+
+}
---
+++ lab3/lab3_2/springboot_mysql/src/test/java/com/ies/ex2/SpringbootMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ies.ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
